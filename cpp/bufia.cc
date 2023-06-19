eb866870acc955e3de9bbd0dceb760332a4845c0
Logan <loganswan37@gmail.com>
2023-06-19 18:28:51 -0400

    manager/worker flow draft

--- cpp/bufia.cc
+++ cpp/bufia.cc
@@ -111,149 +111,143 @@ int main(int argc, char **argv) {
 
 	std:: cout << "begin MPI" << std::endl;
 
-	int rank, size;
+	int rank, num_procs;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
 	//std::cout << "I am " << rank << " of " << size << "\n";
 	
+	char arr[start.bundles.size()][start.bundles.at(0).size()];
+
 	if(rank==0) {
 		// MANAGER flow
+		set<int> active_procs;
+
+		while(queue.size() < num_procs) {
+			Factor current = queue.front();
+			queue.pop_front();
+			// if this factor is already covered by current constraints, skip
+			bool covers = Covers(constraints, current);
+			if(covers) continue;
+
+			// if something that matches this factor is in the positive data,
+			// get all applicable child factors and add to queue
+			bool contains = Contains(positive_data[current.bundles.size()], current);
+
+			if(contains) {
+				list<Factor> next_factors = current.getNextFactors(alphabet, 
+					MAX_FACTOR_WIDTH, MAX_FEATURES_PER_BUNDLE);
+				next_factors.remove_if([constraints](Factor fac){
+					return Contains(constraints, fac);
+				});
+
+				queue.splice(queue.end(), next_factors);
+			} else {
+				constraints.push_back(current);
+				continue;
+			}
+		}
+
+
+		// send work to everyone, add all to active_procs
+		for(int i=1; i<num_procs; ++i) {
+			Factor a = queue.front();
+			queue.pop_front();
+			// convert Factor.bundles to array to send
+			// char arr[a.bundles.size()][a.bundles.at(0).size()];
+			for(int i=0; i<a.bundles.size(); ++i) {
+				std::copy(a.bundles.at(i).begin(), a.bundles.at(i).end(), arr[i]);
+			}
+			MPI_Send(&arr, sizeof(arr), MPI_BYTE, i, 0, MPI_COMM_WORLD);
+			active_procs.insert(i);
+		}
+
 		bool done = false;
 		while(!done) {
-			std::cout << "manager" << std::endl;
-			// get results from all workers
-			// update constraints
-			// add to queue
-			if(queue.empty()) { // && all workers finished
-				done = true;
-				// send done message to workers
+			//std::cout << "manager" << std::endl;
+			// get results from any worker
+			MPI_Status status;
+			MPI_Recv(&arr, sizeof(arr), MPI_BYTE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			active_procs.erase(status.MPI_SOURCE);
+			//std::cout<< "manager, message from: " << status.MPI_SOURCE << std::endl;
+			Factor fac = start;
+			for(int i=0; i<fac.bundles.size(); ++i) {
+				fac.bundles[i] = vector<char>(arr[i], arr[i] + (sizeof(arr[i])/ sizeof(arr[i][0])));
 			}
-			// send next n in queue to any proceses that finished
-			for (int i=1; i<size; i++) {
-				if(!queue.empty()) {
-					Factor a = queue.front();
-					// convert Factor.bundles to array to send
-					char arr[a.bundles.size()][a.bundles.at(0).size()];
-					for(int i=0; i<a.bundles.size(); ++i) {
-						std::copy(a.bundles.at(i).begin(), a.bundles.at(i).end(), arr[i]);
-					}
-					MPI_Send(&arr, sizeof(arr), MPI_BYTE, i, 0, MPI_COMM_WORLD);
-	  		}
-	  	}
-	  	done = true;
+			if(status.MPI_TAG == 0) {
+				// fac not found in positive data. add as constraint.
+				constraints.push_back(fac);
+				std::cout << "manager, constraint: " << Display(fac, feature_order) << std::endl;
+				//if(constraints.size() > 10) done = true;
+			} else {
+				//std::cout << "manager, found: " << fac.toString() << std::endl;
+				// found. add next factors to queue.
+				list<Factor> next_factors = fac.getNextFactors(alphabet, 
+				MAX_FACTOR_WIDTH, MAX_FEATURES_PER_BUNDLE);
+				next_factors.remove_if([constraints](Factor fac){
+					return Covers(constraints, fac);
+				});
+
+				queue.splice(queue.end(), next_factors);
+			}
+
+			if(queue.empty()) {
+				if(active_procs.empty()) done = true; 
+				continue;
+			}
+
+			// send next n in queue to whatever process finished
+			Factor a = queue.front();
+			queue.pop_front();
+			// convert Factor.bundles to array to send
+			for(int i=0; i<a.bundles.size(); ++i) {
+				std::copy(a.bundles.at(i).begin(), a.bundles.at(i).end(), arr[i]);
+			}
+			MPI_Send(&arr, sizeof(arr), MPI_BYTE, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+			active_procs.insert(status.MPI_SOURCE);
+		}
+
+		// send done message to workers
+		// add enum for tags?
+		for(int i=0; i<num_procs; ++i){
+			MPI_Send(&arr, sizeof(arr), MPI_BYTE, i, /*tag=*/1, MPI_COMM_WORLD);
 		}
 	} else {
 		// WORKER flow
 		bool done = false;
 		while(!done) {
-			std::cout << "worker" << std::endl;
+			//std::cout << "worker" << std::endl;
 			// recieve work from manager
 			MPI_Status status;
-			char arr[start.bundles.size()][start.bundles.at(0).size()];
-			MPI_Recv(&arr, sizeof(arr), MPI_BYTE, 0, 0, MPI_COMM_WORLD, &status);
-			std::cout << "Rank: " << rank << ", recieved: " << sizeof(arr) << std::endl;
+			MPI_Recv(&arr, sizeof(arr), MPI_BYTE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+			if(status.MPI_TAG == 1) break;
+
+
+
 			// Convert bundle array back into Factor
 			Factor fac = start;
 			for(int i=0; i<fac.bundles.size(); ++i) {
 				fac.bundles[i] = vector<char>(arr[i], arr[i] + (sizeof(arr[i])/ sizeof(arr[i][0])));
 			}
-			std::cout << "rank: " << rank << " factor: " << fac.toString() << std::endl;
-			done=true;
-			// if message == done, done=true, break
+			//std::cout << "rank: " << rank << " factor: " << fac.toString() << std::endl;
+
 			// compute result
-			// send result to manager
+			if(Contains(positive_data[fac.bundles.size()], fac)) {
+				MPI_Send(&arr, sizeof(arr), MPI_BYTE, 0, /*tag=*/1, MPI_COMM_WORLD);
+			} else {
+				MPI_Send(&arr, sizeof(arr), MPI_BYTE, 0, /*tag=*/0, MPI_COMM_WORLD);
+			}
 		}
 	}
 
 	MPI_Finalize();
 	return 0;
   // End MPI section
-	set<vector<string>> banned_ngrams;
+	// set<vector<string>> banned_ngrams;
 
-	double total_covers = 0;
-	double total_contains = 0;
-	double total_extension_check = 0;
-	double gen_ngrams_time = 0;
-	double check_redundant_time = 0;
-
-	while(!queue.empty()) {
-		Factor current = queue.front();
-		queue.pop_front();
-		// if this factor is already covered by current constraints, skip
-		timespec begin;
-		if(DEBUG_MODE) clock_gettime(CLOCK_REALTIME, &begin);
-		bool covers = Covers(constraints, current);
-		if(DEBUG_MODE) total_covers += diff_timespec(nullptr, &begin);
-
-		if(covers) continue;
-
-		// if something that matches this factor is in the positive data,
-		// get all applicable child factors and add to queue
-		if(DEBUG_MODE) clock_gettime(CLOCK_REALTIME, &begin);
-		bool contains = Contains(positive_data[current.bundles.size()], current);
-		if(DEBUG_MODE) total_contains += diff_timespec(nullptr, &begin);
-
-		if(contains) {
-			list<Factor> next_factors = current.getNextFactors(alphabet, 
-				MAX_FACTOR_WIDTH, MAX_FEATURES_PER_BUNDLE);
-			next_factors.remove_if([constraints](Factor fac){
-				return Contains(constraints, fac);
-			});
-
-			queue.splice(queue.end(), next_factors);
-		} else {
-			if(ABDUCTIVE_PRINCIPLE == 0) {
-				constraints.push_back(current);
-				continue;
-			}
-
-			// if Abductive principle = 1, check whether constraint extends the grammar
-			if(DEBUG_MODE) clock_gettime(CLOCK_REALTIME, &begin);
-			vector<vector<string>> ngrams = ComputeGeneratedNGrams(current, alphabet);
-			timespec fin_ngrams_time;
-			if(DEBUG_MODE) clock_gettime(CLOCK_REALTIME, &fin_ngrams_time);
-			if(DEBUG_MODE) gen_ngrams_time += diff_timespec(&fin_ngrams_time, &begin);
-			bool added_ngrams = false;
-			for(const auto& ngram : ngrams){
-				bool redundant = false;
-				// check all substrings to see if those are already banned
-				for(int i = 1; i<ngram.size(); i++){
-					if(redundant) break;
-					for(int offset = 0; offset <= ngram.size()-i; offset++){
-						// check if ngram[offset:offset+i] is in banned_ngrams
-						vector<string> slice = 
-							vector<string>(ngram.begin()+offset, ngram.begin() + offset + i);
-						if(banned_ngrams.find(slice) != banned_ngrams.end()) {
-							redundant = true;
-							break;
-						}
-					}
-				}
-				if(!redundant && banned_ngrams.insert(ngram).second == true) {
-					added_ngrams = true;
-				}
-			}
-			if(DEBUG_MODE) check_redundant_time += diff_timespec(nullptr, &fin_ngrams_time);
-			if(DEBUG_MODE) total_extension_check += diff_timespec(nullptr, &begin);
-			if(added_ngrams) {
-				constraints.push_back(current);
-				if(DEBUG_MODE) std::cout << Display(current, feature_order) << std::endl;
-			}
-		}
-	}
-
-	if(DEBUG_MODE){
-		std::cout << "Total seconds on Covers(): " << total_covers << std::endl;
-		std::cout << "Total seconds on Contains(): " << total_contains << std::endl;
-		std::cout << "Total seconds on extension checking: " << total_extension_check << std::endl;
-		std::cout << "Time generating NGrams: " << gen_ngrams_time << std::endl;
-		std::cout << "Time checking ngram redundancy: " << check_redundant_time << std::endl;
-		std::cout << "Total time: " << diff_timespec(nullptr, &start_time) << std::endl;
-	} else {
-		for(Factor const& constraint : constraints){
-			std::cout << Display(constraint, feature_order) << std::endl;
-		}
+	
+	for(Factor const& constraint : constraints){
+		std::cout << Display(constraint, feature_order) << std::endl;
 	}
 
 	return 0;
